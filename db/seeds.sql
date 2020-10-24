2d8652060f37bcae2ddad76169c3b32b7f2ee130
Tom Chestnut <tchestnut85@gmail.com>
2020-10-23 23:49:32 -0400

    created directories and files, and made sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name)
+VALUES  
+('EHS'),
+('Research Dev'),
+('Process Dev'),
+('Manufacturing'),
+('Corporate');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  
+('ehs associate'),
+('ehs manager')
+('research scientist'),
+('research manager'),
+('process scientist'),
+('process manager'),
+('operations associate'),
+('ops manager'),
+('salesperson'),
+('CEO');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Falindil', 'Palethorn'),
